3729274bec2dda8cbc54145d8ce64e3245c56bac
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-03 15:00:20 +0000

    updated server.js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,24 @@
 import express from "express";
 import cors from "cors";
-import listEndpoints from "express-list-endpoints";
+import { testConnection, connectDB } from "../src/db/index.js";
 import productsRouter from "./services/products/index.js";
 import reviewsRouter from "./services/reviews/index.js";
-import createDefaultTables from "./db/create-tables.js";
-import { join } from "path";
+
 
 const server = express();
 
-server.use(cors("*"));
+const port = process.env;
+
+server.use(cors());
+
 server.use(express.json());
 
-const publicFolderPath = join(process.cwd(), "public");
-
-
-server.use(express.static(publicFolderPath));
 server.use("/products", productsRouter);
 server.use("/reviews", reviewsRouter);
 
-const port = 3001;
-
-console.table(listEndpoints(server));
 
 server.listen(port, () => {
-    console.log("server on port:", port);
+  console.log("server on port:", port);
+  await testConnection(); // confirms connection is possible
+  await connectDB(); // connects
   });

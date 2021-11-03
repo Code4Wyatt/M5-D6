43ef400e630459ba1134606fcc2df4fe7cab21db
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-03 09:18:52 +0000

    update

---
+++ src/db/connect.js
@@ -0,0 +1,18 @@
+import pg from "pg";
+
+/**
+ *
+ *  pool is place where we keep idle connections
+ *
+ *  idle : connection is not in use, authenticated waiting for to be used ,
+ *
+ *  with pool you dont have to do handshake in every database query.
+ *
+ * you do once and use it.
+ *
+ * ref : https://node-postgres.com/features/pooling
+ */
+
+const pool = new pg.Pool();
+
+export default pool;

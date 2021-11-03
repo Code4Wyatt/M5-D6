596aabfcae1797d714e0a349c39cba34acc4ad31
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-03 14:53:31 +0000

    added products index.js

--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -20,4 +20,42 @@ router.route("/").get(async (req, res, next) => {
     console.log(error);
     next(error);
   }
-};
+});
+
+router.route("/:id").get(async (req, res, next) => {
+  try {
+    const review = await Review.findByPk(req.params.id); // find By Priority Key, passing in the priority key via the req params
+    res.send(review);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}).put(async (req, res, next) => {
+  try {
+    const updated = await Product.update(req.body, {  // update the request body
+      where: {                  // at the location
+        id: req.params.id,      // where the id matches that of the id in the req.params
+      },
+      returning: true,     // what does this do?
+    });
+    res.send(updated);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+).delete(async (req, res, next) => {
+  try {
+    const productRow = await Review.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send(productRow);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+export default router;
